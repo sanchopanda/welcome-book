acb27520d556aa83c4676ce77ad20fd9f7b225be
Ivanov Alexander <a.ivanov@uplab.ru>
2020-08-22 16:42:09 +0300

    стилизация первой страницы

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,6 @@
-/*
-import { openMenu } from './burger.js';
-import { scrollDown } from './scrollDown.js';
-import { togglePopup } from './popup.js';
-import { catalogSlider } from './catalogSlider.js';
-import { exampleSlider } from './exampleSlider.js';
-import { activeMenuItem } from './activeMenuItem.js';
 
-openMenu();
-
-scrollDown();
-
-togglePopup();
-
-catalogSlider();
-
-exampleSlider();
-
-activeMenuItem();
+import { scaleHtml } from './scale-html.js';
 
 
-
-*/
+scaleHtml();
 
