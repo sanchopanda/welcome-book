065cae2aae62c0db38e2b7931cd940cbc9810c89
Ivanov Alexander <a.ivanov@uplab.ru>
2020-09-02 23:01:22 +0300

    Добавить переход на страницу о компании по кнопке

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,10 @@
 import { scaleHtml } from './scale-html.js';
 import { slidePage } from './slide-page.js';
 import { stepSlide, startFillingProgress } from './step-slide.js'
+import { sectionToggler } from './section-toggler.js'
 
 
 scaleHtml();
 slidePage();
 stepSlide();
+sectionToggler();
