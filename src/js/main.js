50aef81d18fdffea85154253b015de09ed8409a2
Ivanov Alexander <a.ivanov@uplab.ru>
2020-09-02 23:02:16 +0300

    Revert "Добавить переход на страницу о компании по кнопке"
    This reverts commit 065cae2aae62c0db38e2b7931cd940cbc9810c89.

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,8 @@
 import { scaleHtml } from './scale-html.js';
 import { slidePage } from './slide-page.js';
 import { stepSlide, startFillingProgress } from './step-slide.js'
-import { sectionToggler } from './section-toggler.js'
 
 
 scaleHtml();
 slidePage();
 stepSlide();
-sectionToggler();
