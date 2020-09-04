c4cf77611e9c6533d34cdf2201b0aa57e0acbb9c
Ivanov Alexander <a.ivanov@uplab.ru>
2020-09-04 18:10:49 +0300

    Добавить парралакс на странице приветствия и текст внизу

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import { slidePage } from './slide-page.js';
 import { stepSlide, startFillingProgress } from './step-slide.js'
 import { sectionToggler } from './section-toggler.js'
 import { infoSlidePage } from './info-slide-page.js'
+import { parallaxBg } from './parallax-bg.js'
 
 
 scaleHtml();
@@ -11,3 +12,4 @@ slidePage();
 stepSlide();
 sectionToggler();
 infoSlidePage();
+parallaxBg();
