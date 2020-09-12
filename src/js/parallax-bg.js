77657328d9b63a81c2187e0bb983c6a9b934638c
Ivanov Alexander <a.ivanov@uplab.ru>
2020-09-12 19:25:45 +0300

    Перевернуть социалки

--- src/js/parallax-bg.js
+++ src/js/parallax-bg.js
@@ -1,10 +1,12 @@
 export function parallaxBg() {
     let bg = document.querySelector('.mouse-parallax-bg');
+    let bgReverse = document.querySelector('.mouse-parallax-bg-reverse');
     window.addEventListener('mousemove', function(e) {
         let x = e.clientX / window.innerWidth;
         let y = e.clientY / window.innerHeight;  
         bg.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
-    });
+        bgReverse.style.backgroundPosition = x * 20 + 'px,' + y * 20 + 'px';
+    });  
 }
 
 
