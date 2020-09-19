6d327be80cd86c894996c59df4fee3572bb0c0f1
Ivanov Alexander <a.ivanov@uplab.ru>
2020-09-19 11:09:30 +0300

    паралакс библиотекой

--- src/js/parallax-bg.js
+++ src/js/parallax-bg.js
@@ -1,12 +1,18 @@
+
+import Parallax from 'parallax-js'
+
 export function parallaxBg() {
-    let bg = document.querySelector('.mouse-parallax-bg');
-    let bgReverse = document.querySelector('.mouse-parallax-bg-reverse');
+   let bg = document.querySelector('.mouse-parallax-bg');
+    let bgReverse = document.querySelector('.parallax-scene');
+     /*
     window.addEventListener('mousemove', function(e) {
         let x = e.clientX / window.innerWidth;
         let y = e.clientY / window.innerHeight;  
         bg.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
         bgReverse.style.backgroundPosition = x * 20 + 'px,' + y * 20 + 'px';
-    });  
+    });  */
+
+    var parallaxInstance = new Parallax(bgReverse);
 }
 
 
